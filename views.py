79e8d6a052c18ca268eef90e7675cda152073930
AK <github1em@gmail.com>
2013-09-28 22:34:40 -0500

    Randomized the positions each firefly initially appears at

--- views.py
+++ views.py
@@ -10,17 +10,17 @@ app.config.from_pyfile('settings.py')
 def home():
     total_fireflies = randint(12, 50)
     color_seed = randint(1, 255)
-    if color_seed % 51 == 0:
+    if color_seed % 42 == 0:
         color = "rgb(143, 100, 255)"  # light violet
-    elif color_seed % 42 == 0:
-        color = "rgb(75, 100, 130)"  # light indigo?
     elif color_seed % 36 == 0:
-        color = "rgb(100, 100, 255)"  # light blue
+        color = "rgb(75, 100, 130)"  # light indigo?
     elif color_seed % 31 == 0:
-        color = "rgb(100, 255, 100)"  # light green
+        color = "rgb(100, 100, 255)"  # light blue
     elif color_seed % 28 == 0:
-        color = "rgb(255, 127, 100)"  # light orange
+        color = "rgb(100, 255, 100)"  # light green
     elif color_seed % 25 == 0:
+        color = "rgb(255, 127, 100)"  # light orange
+    elif color_seed % 23 == 0:
         color = "rgb(255, 100, 100)"  # light red
     else:
         color = "rgb(255, 255, 50)"  # default is yellow
